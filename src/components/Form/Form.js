fa1cd4b856f17ce85c11bea46bce5aff153820a0
Darren Tebo <darrentebo83@outlook.com>
2020-06-13 22:51:48 -0400

    added all form sections and began adding size values to the size dropdown

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -20,11 +20,28 @@ const PizzaForm = () => {
             </section>
             <FormGroup>
                 <Label for='size' />
-                <Input type="select" id='size' name='size' />
+                <Input type="select" id='size' name='size'>
+                    <option value=''>Select</option>
+                    <option value='small'>Small</option>
+                    <option value='medium'>Medium</option>
+                    <option value='large'>Large</option>
+                    <option value='extralarge'>Extra Large</option>
+                </Input>
             </FormGroup>
             <section className='input-group-header'>
-                <h2>Choice of Size</h2>
+                <h2>Choice of Sauce</h2>
                 <h3>Required</h3>
+            </section>
+            <section className='input-group-header'>
+                <h2>Add Toppings</h2>
+                <h3>Choose up to 10</h3>
+            </section>
+            <section className='input-group-header'>
+                <h2>Choice of Substitute</h2>
+                <h3>Choose up to 1</h3>
+            </section>
+            <section className='input-group-header'>
+                <h2>Special Instructions</h2>
             </section>
         </Form>
     );
