f77d70eff47624a535f1d72c208c14626013cd39
Darren Tebo <darrentebo83@outlook.com>
2020-06-13 23:33:15 -0400

    added form section inputs and styled them

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -3,6 +3,10 @@ import React from 'react';
 import './Form.css';
 
 import {
+    Container,
+    Row,
+    Col,
+    CustomInput,
     Button,
     Form,
     FormGroup,
@@ -32,14 +36,133 @@ const PizzaForm = () => {
                 <h2>Choice of Sauce</h2>
                 <h3>Required</h3>
             </section>
+            <FormGroup check>
+                <Label check>
+                    <Input type='radio' name='sauce' id='sauce' />{' '}
+                    Original Red
+                </Label>
+            </FormGroup>
+            <FormGroup check>
+                <Label check>
+                    <Input type='radio' name='sauce' id='sauce' />{' '}
+                    Garlic Ranch
+                </Label>
+            </FormGroup>
+            <FormGroup check>
+                <Label check>
+                    <Input type='radio' name='sauce' id='sauce' />{' '}
+                    BBQ Sauce
+                </Label>
+            </FormGroup>
+            <FormGroup check>
+                <Label check>
+                    <Input type='radio' name='sauce' id='sauce' />{' '}
+                    Spinach Alfredo
+                </Label>
+            </FormGroup>
             <section className='input-group-header'>
                 <h2>Add Toppings</h2>
                 <h3>Choose up to 10</h3>
             </section>
+            <Container>
+                <Row>
+                    <Col>
+                        <FormGroup check>
+                            <Label check>
+                                <Input type='checkbox' />{' '}
+                                Pepperoni
+                            </Label>
+                        </FormGroup>
+                        <FormGroup check>
+                            <Label check>
+                                <Input type='checkbox' />{' '}
+                                Sausage
+                            </Label>
+                        </FormGroup>
+                        <FormGroup check>
+                            <Label check>
+                                <Input type='checkbox' />{' '}
+                                Canadian Bacon
+                            </Label>
+                        </FormGroup>
+                        <FormGroup check>
+                            <Label check>
+                                <Input type='checkbox' />{' '}
+                                Spicy Italian Sausage
+                            </Label>
+                        </FormGroup>
+                        <FormGroup check>
+                            <Label check>
+                                <Input type='checkbox' />{' '}
+                                Grilled Chicken
+                            </Label>
+                        </FormGroup>
+                        <FormGroup check>
+                            <Label check>
+                                <Input type='checkbox' />{' '}
+                                Onions
+                            </Label>
+                        </FormGroup>
+                        <FormGroup check>
+                            <Label check>
+                                <Input type='checkbox' />{' '}
+                                Green Pepper
+                            </Label>
+                        </FormGroup>
+                    </Col>
+                    <Col>
+                        <FormGroup check>
+                            <Label check>
+                                <Input type='checkbox' />{' '}
+                                Diced Tomatos
+                            </Label>
+                        </FormGroup>
+                        <FormGroup check>
+                            <Label check>
+                                <Input type='checkbox' />{' '}
+                                Black Olives
+                            </Label>
+                        </FormGroup>
+                        <FormGroup check>
+                            <Label check>
+                                <Input type='checkbox' />{' '}
+                                Roasted Garlic
+                            </Label>
+                        </FormGroup>
+                        <FormGroup check>
+                            <Label check>
+                                <Input type='checkbox' />{' '}
+                                Artichoke Hearts
+                            </Label>
+                        </FormGroup>
+                        <FormGroup check>
+                            <Label check>
+                                <Input type='checkbox' />{' '}
+                                Three Cheese
+                            </Label>
+                        </FormGroup>
+                        <FormGroup check>
+                            <Label check>
+                                <Input type='checkbox' />{' '}
+                                Pineapple
+                            </Label>
+                        </FormGroup>
+                        <FormGroup check>
+                            <Label check>
+                                <Input type='checkbox' />{' '}
+                                Extra Cheese
+                            </Label>
+                        </FormGroup>
+                    </Col>
+                </Row>
+            </Container>
             <section className='input-group-header'>
                 <h2>Choice of Substitute</h2>
                 <h3>Choose up to 1</h3>
             </section>
+            <FormGroup>
+                <CustomInput type='switch' id='gluten' label='Gluten Free Crust (+ $1.00)' />
+            </FormGroup>
             <section className='input-group-header'>
                 <h2>Special Instructions</h2>
             </section>
